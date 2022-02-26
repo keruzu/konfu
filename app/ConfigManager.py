efb02c2cbf6f5f5f6f5f78a9f4b1911d90e19c8d
Kells Kearney <keruzu@gmail.com>
2022-02-26 11:44:09 -0500

    Initial git management of files

--- app/ConfigManager.py
+++ app/ConfigManager.py
@@ -14,18 +14,25 @@ class ConfigManager(object):
             from defaultLogger import defaultLogger
             self.log = defaultLogger()
 
-        self.repo = None
-        # self.repo = self.getRepo(path)
+        self.decoding_format = 'utf-8'
 
-    def getFile(self, source, path):
+        # Note: can't use any path, otherwise React/NodeJS will not be able to read
+        self.repo_path = 'src/data'
+        self.repo = self.getRepo(self.repo_path)
+
+    def getFile(self, source, path_type):
         '''
-        Retrieve a schema or configuration file, based on path
+        Retrieve a schema or configuration file, based on path_type
         '''
         sane = self.pathClean(source)
         if sane is None:
             return HTTPStatus.BAD_REQUEST, "Unable to parse source data name: {source}"
 
-        filename = f'src/{path}/{sane}.json'
+        if path_type == 'data':
+            filename = f'{self.repo_path}/{sane}.json'
+        else:
+            filename = f'src/{path_type}/{sane}.json'
+
         try:
             with open(filename, 'r') as fd:
                 data = fd.read()
@@ -44,7 +51,11 @@ class ConfigManager(object):
         except Exception:
             pass
 
-    def saveConfig(self, request, config):
+    def saveConfig(self, request, user_supplied_filename, msg='Automated commit'):
+        '''
+        Save configuration (Flask request object) from the user
+        to a configuration file
+        '''
         try:
             raw_data = request.json
         except Exception as ex:
@@ -53,16 +64,18 @@ class ConfigManager(object):
         if not raw_data:
             return HTTPStatus.BAD_REQUEST, "Empty JSON message"
 
-        saneConfig = self.pathClean(config)
-        filename = f'src/data/{saneConfig}.json'
+        saneConfig = self.pathClean(user_supplied_filename)
+        filename = f'{self.repo_path}/{saneConfig}.json'
         try:
-            configData = request.data.decode('utf-8')
+            configData = request.data.decode(self.decoding_format)
             with open(filename, 'w') as fd:
                 fd.write(configData)
-            status = dict(config=config, status="Saved file")
+            status = dict(config=filename, status="Saved file")
         except Exception as ex:
             self.log.error("Unable to save JSON config", extra=dict(filename=filename, ex_msg=str(ex)))
             return HTTPStatus.INTERNAL_SERVER_ERROR, f"Unable to save JSON config {filename}: {ex}"
+
+        self.addVersion(filename, msg=msg)
         return HTTPStatus.OK, status
 
     def getRepo(self, path):
